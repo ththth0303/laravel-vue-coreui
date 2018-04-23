879b9a6a183e95d999b945d1e68fe98ee38969e2
thangtt <trungthangngo@gmail.com>
2018-04-23 14:40:58 +0700

    init admin template

---
+++ resources/assets/js/admin/_nav.js
@@ -0,0 +1,66 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer'
+    },
+    {
+      name: 'Dashboard',
+      url: '/dashboard1',
+      icon: 'icon-speedometer',
+      children: [
+        {
+          name: 'Flags',
+          url: '/icons/flags',
+          icon: 'icon-star',
+          badge: {
+            variant: 'success',
+            text: 'NEW'
+          }
+        },
+        {
+          name: 'Font Awesome',
+          url: '/icons/font-awesome',
+          icon: 'icon-star',
+          badge: {
+            variant: 'secondary',
+            text: '4.7'
+          }
+        },
+        {
+          name: 'Simple Line Icons',
+          url: '/icons/simple-line-icons',
+          icon: 'icon-star'
+        }
+      ]
+    },
+    {
+      name: 'Charts',
+      url: '/charts',
+      icon: 'icon-pie-chart'
+    },
+    {
+      name: 'Notifications',
+      url: '/notifications',
+      icon: 'icon-bell',
+      children: [
+        {
+          name: 'Alerts',
+          url: '/notifications/alerts',
+          icon: 'icon-bell'
+        },
+        {
+          name: 'Badges',
+          url: '/notifications/badges',
+          icon: 'icon-bell'
+        },
+        {
+          name: 'Modals',
+          url: '/notifications/modals',
+          icon: 'icon-bell'
+        }
+      ]
+    }
+  ]
+}
